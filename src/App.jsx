5bc6a0797e6bd6bfb30e602cd6b50728b28246a3
Hsu Thinzar <hsumeow1210@gmail.com>
2024-08-25 01:02:30 +0630

    header_ver0.01

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import { Box } from '@mui/material';
+function App() {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const drawerWidth = 240;
+const navItems = ["Discover", "Trips", "Review"];
+
+export default function Header(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Box sx={{ my: 2 }}>
+        <img
+          src="/images/BurmaOdysseyLogo.png"
+          alt="Burma Odyssey"
+          width="100"
+        />
+      </Box>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{ bgcolor: "#fff" }}>
+        <Toolbar>
+          <IconButton
+            color="#000"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "flex" },
+              alignItems: "center",
+            }}
+          >
+            <img
+              src="/images/BurmaOdysseyLogo.png"
+              alt="Burma Odyssey"
+              width={"100px"}
+            />
+          </Box>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item}
+                sx={{
+                  color: "#000",
+                  bgcolor: "#ffb4af",
+                  mx: 2,
+                  py: 1,
+                  px: 4,
+                  borderRadius: "1rem",
+                }}
+              >
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
+}
